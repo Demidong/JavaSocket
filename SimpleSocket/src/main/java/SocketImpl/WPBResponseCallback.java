afbf4df7d54e9ee459222f55cb808a62e750fd93
qiuping.dong <qiuping.dong@baidao.com>
2016-10-20 17:22:06 +0800

    测试实现

---
+++ SimpleSocket/src/main/java/SocketImpl/WPBResponseCallback.java
@@ -0,0 +1,14 @@
+package SocketImpl;
+
+
+import demisocket.tcp.Packet;
+
+/**
+ * Created by demi on 16/10/19.
+ */
+public abstract class WPBResponseCallback {
+
+
+    public abstract void onSucess(Packet packet);
+
+}
